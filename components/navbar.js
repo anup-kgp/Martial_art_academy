70e19bee95bd2101a3420eb41fba72cf505a83f5
anup-kgp <dasanup1999.com>
2022-12-02 15:16:53 +0530

    first commit

---
+++ components/navbar.js
@@ -0,0 +1,16 @@
+function navbar(){
+    return` 
+    <a class="nav_link" href="index.html">
+        <h3>Home</h3>
+    </a>
+   
+    <a class="nav_link" href="gallery.html"><h3>Gallery</h3></a>
+    <a class="nav_link" href="training.html"><h3>Our Training</h3>  
+    </a>
+    <a class="nav_link" href="Instructor.html"><h3>Instructors</h3></a>
+    <a class="nav_link" href="Students.html"><h3>Students</h3></a>
+    <a class="nav_link" href="Contact.html"><h3>About us</h3></a>
+    <a class="nav_link" href="login.html"><h3>Login</h3></a>
+ `
+}
+export {navbar}
---
+++ script/import_navbar.js
@@ -0,0 +1,10 @@
+import { navbar } from "../components/navbar.js";
+// const mydiv=document.createElement("div")
+// mydiv.setAttribute("id","navbar");
+// document.body.append(mydiv)
+// mydiv.innerHTML=navbar()
+
+let nav=document.getElementById("navbar");
+nav.innerHTML=navbar()
+
+
